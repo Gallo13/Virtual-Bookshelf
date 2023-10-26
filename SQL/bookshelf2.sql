cffc8db8739397c29e608b6822e770db73cd0b30
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2023-10-26 17:48:17 -0400

    fixed drop table if exists

--- SQL/bookshelf2.sql
+++ SQL/bookshelf2.sql
@@ -2,17 +2,15 @@ create database bookshelf2;
 
 use bookshelf2;
 
-DROP TABLE IF EXISTS books, authors, book_author, publisher, book_publisher, book_courses, genre, book_genre;  /* cleanup old stuff */
+DROP TABLE IF EXISTS books, authors, book_author, publisher, book_publisher, genre, book_genre, series, book_series;  /* cleanup old stuff */
 
 create table books
-  (bID		 	char(36) NOT NULL PRIMARY KEY,	 
-   title	 	varchar(225) NOT NULL,		
-   pages	 	smallint(5) NOT NULL, 
-   rating	 	char(1),
-   date_added		date NOT NULL,
-   published_date	date NOT NULL,
-   number_in_series 	int(3));
-
+  (bID		 char(36) NOT NULL PRIMARY KEY,	 
+   title	 varchar(225) NOT NULL,		
+   pages	 smallint(5) NOT NULL, 
+   rating	 char(1),
+   date_added	 date NOT NULL,
+   number_in_series 	 int(3));
 
 create table authors
   (aID	     	char(36) PRIMARY KEY,
@@ -25,7 +23,6 @@ create table book_author
    CONSTRAINT FOREIGN KEY (bID) REFERENCES books(bID),
    CONSTRAINT FOREIGN KEY (aID) REFERENCES authors(aID));
 
-
 create table publisher
   (pID		char(36) NOT NULL PRIMARY KEY,
    publisher	varchar(64) NOT NULL);
@@ -35,12 +32,6 @@ create table book_publisher
    pID		char(36) NOT NULL,
    CONSTRAINT FOREIGN KEY (bID) REFERENCES books(bID),
    CONSTRAINT FOREIGN KEY (pID) REFERENCES publisher(pID));
-
-create table book_courses
-  (bID		char(36) NOT NULL,
-   dept		char(3) NOT NULL,
-   course	char(32),
-   CONSTRAINT FOREIGN KEY (bID) REFERENCES books(bID));
 
 create table genre
   (gID		char(36)  NOT NULL PRIMARY KEY,
@@ -54,13 +45,13 @@ create table book_genre
 
 create table series
 (
-    sID char(36) NOT NULL PRIMARY KEY,
+	sID char(36) NOT NULL PRIMARY KEY,
     seriesName varchar(225) NOT NULL
 );
 
 create table book_series
 (
-    bID		char(36) NOT NULL,
+	bID		char(36) NOT NULL,
     sID		char(36) NOT NULL,
     CONSTRAINT FOREIGN KEY (bID) REFERENCES books(bID),
     CONSTRAINT FOREIGN KEY (sID) REFERENCES series(sID)
